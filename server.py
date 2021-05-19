eea0db91f76ac2ee417c43bcc4d726aa24ea8539
Andrew Lee <avllee21@Andrews-MBP-5.cable.rcn.com>
2021-05-19 07:32:44 -0400

    changed server

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import socket
-import threading
-from note import Note
-from course import Course
 import time
+import threading
+from course import Course
+from note import Note
 
 
 class Server:
@@ -15,44 +15,49 @@ class Server:
         self.username = name
         self.course = course
         self.note = note
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('127.0.0.1', course.course_port))
-        sock.listen(1)
+
+
+        # bind the server socket to a specific address and a course port
+        serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serversock.bind(('127.0.0.1', course.course_port))
+        serversock.listen(1)
+
 
         print(self.username + " is now the acting server of this room!")
-
+        
+        # start accepting connections from clientsockets
         while True:
-            c, a = sock.accept()
-            cThread = threading.Thread(target=self.handler, args=(c, a))
+            clientsocket, clientaddress = serversock.accept()
+            cThread = threading.Thread(target=self.handler, args=(clientsocket, clientaddress))
             cThread.daemon = True
             cThread.start()
-            self.connections.append(c)
-            self.peers.append(a[0] + ':' + str(a[1]))
-            print(str(a[0]) + ':' + str(a[1]), "has connected")
+            self.connections.append(clientsocket)
+            self.peers.append(clientaddress[0] + ':' + str(clientaddress[1]))
+            print(str(clientaddress[0]) + ':' + str(clientaddress[1]), "has connected")
             self.sendPeers()
             
 
-    def handler(self, c, a):
+    def handler(self, clientsocket, clientaddress):
         while True:
-            data = c.recv(1024)
+            data = clientsocket.recv(1024)
 
             if not data:
-                print(str(a[0]) + ':' + str(a[1]), "has disconnected")
+                print(str(clientaddress[0]) + ':' + str(clientaddress[1]), "has disconnected")
                 
                 # TODO: Fix the disconnection logic
                 # TODO: Remove the disconnected client from the self.peers_with_name set
-                self.connections.remove(c)
-                self.peers.remove(a[0] + ':' + str(a[1]))
+                self.connections.remove(clientsocket)
+                self.peers.remove(clientaddress[0] + ':' + str(clientaddress[1]))
 
 
 
-                c.close()
+                clientsocket.close()
                 self.sendPeers()
                 break
             
             elif data[0:5] == b'User:':
-                self.peers_with_name.add(str(a[0]) + ':' + str(a[1])+"?"+str(data[5:].decode('UTF-8')))
+                self.peers_with_name.add(str(clientaddress[0]) + ':' + str(clientaddress[1])+"?"+str(data[5:].decode('UTF-8')))
 
                 #Broadcast all the users till now
                 u = ""
@@ -77,7 +82,7 @@ class Server:
 
 
             else:
-                print(a, ":", data.decode('UTF-8'))
+                print(clientaddress, ":", data.decode('UTF-8'))
                 self.note.body = self.note.body + data.decode('UTF-8') + "\n"
                 self.broadcast(data)
 
